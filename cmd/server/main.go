4122a281bf0be40805b06b60f22fd134859f9bb2
SudeepPatel0812 <sudypatel0801@gmail.com>
2026-08-24 23:50:43 +0530

    added postgresql connection

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,17 +5,28 @@ import (
 	"net/http"
 
 	"webhook/internal/config"
+	"webhook/internal/db"
 )
 
 func main() {
 	mux := http.NewServeMux()
 	port := ":8081"
+
 	config.Load()
+	db := db.Ping()
+
 	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("OK"))
 	})
+
+	mux.HandleFunc("/ready", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(db))
+	})
+
 	if err := http.ListenAndServe(port, mux); err != nil {
 		log.Fatalf("failed to start server: %v", err)
 	}
---
+++ internal/db/db.go
@@ -0,0 +1,30 @@
+package db
+
+import (
+	"context"
+	"log"
+	"os"
+	"time"
+
+	"github.com/jackc/pgx/v5"
+)
+
+func Ping() string {
+	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatalf("Error connecting to database: %v", err)
+		return "Error connecting to database"
+	}
+	defer conn.Close(context.Background())
+
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	defer cancel()
+
+	conn.Ping(ctx)
+	if err != nil {
+		log.Fatalf("Error connecting to database: %v", err)
+		return "Error connecting to database"
+	}
+
+	return "Connection to database successful!"
+}
